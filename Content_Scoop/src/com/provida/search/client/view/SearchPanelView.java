9d2b75c7ab2567a22cc58778c81b267e32462354
Anupriya Chhabra <achhabra@xebia.com>
2013-05-15 03:06:45 +0530

    set

--- Content_Scoop/src/com/provida/search/client/view/SearchPanelView.java
+++ Content_Scoop/src/com/provida/search/client/view/SearchPanelView.java
@@ -1,5 +1,6 @@
 package com.provida.search.client.view;
 
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -9,6 +10,7 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.util.tools.shared.StringUtils;
 import com.google.inject.Singleton;
 import com.provida.search.client.presenter.interfaces.ISearchPanelView;
 import com.provida.search.client.presenter.interfaces.ISearchPanelView.ISearchPanelPresenter;
@@ -22,36 +24,39 @@ public class SearchPanelView extends ReverseCompositeView<ISearchPanelPresenter>
 	}
 
 	@UiField
-	TextBox term,numberOfWords;
+	TextBox term,numberOfWords,imageWidth,imageHeight;
 	
 	@UiField
 	Button searchButton;
 	
 	@UiField
-	ListBox imageType,imageSize;
+	ListBox imageType;
 
 	public SearchPanelView() {
 		initWidget(uiBinder.createAndBindUi( this ) );
+		imageType.addItem("ALL");
 		imageType.addItem("JPG");
 		imageType.addItem("PNG");
 		imageType.addItem("GIF");
 		imageType.addItem("BMP");
-		imageSize.addItem("SMALL");
-		imageSize.addItem("MEDIUM");
-		imageSize.addItem("LARGE");
-		imageSize.addItem("EXTRA_LARGE");
+		
 	}	
 	
 	@UiHandler("searchButton")
 	public void onClickSearchButton(ClickEvent e){
 		String imageTypeString = imageType.getValue(imageType.getSelectedIndex());
-		String imageSizeString = imageSize.getValue(imageSize.getSelectedIndex());
+		String imageSize = null;
+		String width = imageWidth.getValue();
+		String height = imageHeight.getValue();
+		if(!"".equals(width) && !"".equals(height)){
+			imageSize = width+"x"+height;
+		}
 		String words = numberOfWords.getValue();
 		if(words== null || "".equals(words)){
 			words="0";
 		}
-		System.out.println("User searched for " +term.getValue()+"  "+imageSizeString+"  "+imageTypeString);
-		presenter.startSearch(term.getValue(),imageTypeString,imageSizeString,Integer.parseInt(words));
+		System.out.println("User searched for " +term.getValue()+"  "+imageSize+"  "+imageTypeString);
+		presenter.startSearch(term.getValue(),imageTypeString,imageSize,Integer.parseInt(words));
 	}
 	
 
